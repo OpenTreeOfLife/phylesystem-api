60a550cdd70a102439072cd4f722e9601124fd50
Jonathan "Duke" Leto <jonathan@leto.net>
2013-09-19 17:16:52 -0700

    Unify the default controller into one file for now and define cache.redis

--- controllers/default.py
+++ controllers/default.py
@@ -1,5 +1,23 @@
-# A simple API method for now so we can start writing + running tests
+from gluon.contrib.redis_cache import RedisCache
+cache.redis = RedisCache('localhost:6379',db=None, debug=True)
 
-def index():
-    session.counter = (session.counter or 0) + 1
-    return dict(counter=session.counter, now=request.now)
+@request.restful()
+
+def api():
+    response.view = 'generic.json'
+    def GET(resource,resource_id):
+        if not resource=='study': raise HTTP(400)
+        # return the correct nexson of study_id
+        return dict()
+    def POST(resource,resource_id):
+        if not resource=='study': raise HTTP(400)
+        # overwrite the nexson of study_id with the POSTed data
+        # 1) verify that it is valid json
+        # 2) Update local treenexus git repo at ../treenexus
+        # 3) See if the hash of the current value of the file matches the hash of the POSTed data. If so, do nothing and return successfully.
+        # 4) If not, overwrite the correct nexson file on disk
+        # 5) Make a git commit with the updated nexson (add as much automated metadata to the commit message as possible)
+        # 6) return successfully
+
+        return dict()
+    return locals()
