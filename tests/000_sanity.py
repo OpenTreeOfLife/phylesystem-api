5688ac724bb319f1fd80692a86eeb16d058afd07
Jonathan "Duke" Leto <jonathan@leto.net>
2013-09-21 09:45:49 -0700

    Add a POST test

--- tests/000_sanity.py
+++ tests/000_sanity.py
@@ -3,15 +3,40 @@ from nose.tools import *
 
 import requests
 
-def test_basic_api_call():
+def test_basic_api_get():
     protocol = 'http://'
     host     = 'localhost'
-    port     = 8000
+    port     = "8000"
     url      = protocol + host + ":" + port + '/v1/study/10.json'
 
-    # ask the API for the NexSON for study 10
+    # ask the API for the study 10 NexSON
     r        = requests.get(url)
 
-    ok_(r.status_code == requests.codes.ok, url + " returns a successful status code")
+    eq_(r.status_code, requests.codes.ok, url + " returns a successful status code")
+
+    eq_(r.headers.get('content-type'), 'application/json', url + "content-type is application/json")
+
+    nexson = r.json()
+
+    assert( len(json) > 3, "Returned NexSON is non-empty" )
+
+def test_basic_api_post():
+    protocol = 'http://'
+    host     = 'localhost'
+    port     = "8000"
+    url      = protocol + host + ":" + port + '/v1/study/10.json'
+
+    api_key  = 'deadbeef'
+    nexson   = '{ "foo": "bar" }'
+
+    # ask the API to overwrite the NexSON for study 10
+    payload = {'key': api_key , 'nexson': nexson}
+    r        = requests.post(url, data=payload)
+
+    eq_(r.status_code, requests.codes.ok, url + " returns a successful status code")
+
+    eq_(r.headers.get('content-type'), 'application/json', url + "content-type is application/json")
 
     json = r.json()
+
+    assert( len(json) > 3, "Returned NexSON is non-empty" )
