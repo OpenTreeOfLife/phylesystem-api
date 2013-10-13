47c000ba21558dccf1dbbe87d003f9d1c9ff6a16
Mark T. Holder <mtholder@gmail.com>
2013-10-12 21:36:48 -0500

    Tests of embedding JSON messages, but broken.
    The refersTo field of the JSON outputting tests is bogus.
    Tests pass, but should not.

---
+++ nexson-validator/run_embed_json_message_tests.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+subdir=single
+total=0
+passed=0
+
+script=scripts/normalize_ot_nexson.py
+echo "testing with ${script}"
+
+# run the test for all files in the input dir of the subdirectory of interest...
+for path in $(ls tests/single/input/*)
+do
+    # set some variables to make the invocation easier to read
+    #
+    filename=$(basename "${path}")
+    input="tests/${subdir}/input/${filename}"
+    output="tests/${subdir}/output/${filename}"
+    reference="tests/${subdir}/expected-embed-json-output/${filename}"
+    echo "testing: python ${script} ${input}"
+    python "${script}" --validate --embed "${input}" > "tests/${subdir}/output/.raw" 2>&1
+    cat "tests/${subdir}/output/.raw" | sed -e '/"dateCreated"/d' | sed -e '/"id": "meta-/d' | sed -e '/"pythonImplementation"/d' | sed -e '/pythonVersion/d' > "${output}"
+    if diff "${output}" "${reference}"
+    then
+        # diff will succeed if the files are identical
+        #
+        echo "Passed"
+        passed=$(expr $passed + 1)
+    else
+        echo "Did not create the expected output!"
+    fi
+    total=$(expr $total + 1)
+done
+
+echo "Passed $passed out of $total tests"
+
+if test $passed -eq $total
+then
+    exit 0
+else
+    exit 1
+fi
