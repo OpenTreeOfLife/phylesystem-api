5e6a299d286670bb800219934b1384a57fdc9145
Jim Allman <jim@ibang.com>
2013-11-01 22:47:00 -0400

    Better example code and sketch for fetch_study

--- modules/github_client.py
+++ modules/github_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
-from github import Github
+import github
+from github import Github, GithubException, BadCredentialsException
 # diagnostic helpers
 from pprint import pprint
 
@@ -15,59 +16,57 @@ except:
     pass
 
 
-def build_common_api_headers(auth_info):
-    if auth_info:
-        oauth_token = auth_info['token'] or 'AUTH_NO_TOKEN'
-    elif in_web2py:
-        oauth_token = current.request.args['auth_token'] or 'ANONYMOUS_VISITOR'
-    else:
-        # mock a fake request with canned values?
-        oauth_token = ''
-
-    return {
-        'User-Agent': 'Open Tree of Life - Study Curation App',
-        'Authentication': ('token %s' % oauth_token),
-
-        # TODO: more headers to use cached results if no change
-        # http://developer.github.com/v3/#conditional-requests
-        ##'If-None-Match': '',
-        ##'If-Modified-Since': '',
-
-        # TODO: if we need to support CORS
-        # http://developer.github.com/v3/#cross-origin-resource-sharing
-
-        # TODO: if we need to fetch JSON-P
-        # http://developer.github.com/v3/#json-p-callbacks
-    }
-
 def fetch_study(study_id, auth_info):
     #headers = build_common_api_headers()
     gh = Github(auth_info.get('token', None))
-    for repo in gh.get_user().get_repos():
-        print repo.name
-        #repo.edit(has_wiki=False)
+    # TODO: use the proper sequence of API calls
+    #   find the 'treenexus' repo (confirm it exists and is available)
+    #   does this user have an existing study (WIP) branch?
+    #   IF SO, fetch latest from this branch
+    #   IF NOT, fetch the 'master' version
 
 
-# add testing or other behavior if run as standalone script
+# if this is run as standalone script, run some initial tests and demos
+# EXAMPLE:   $ github_client.py AUTH_TOKEN    <== unquoted OAuth token
 def main(*args):
-    # args is a tuple of strings passed on the command line
+    # test to see if we're running inside web2py or standalone
+    if in_web2py:
+        if current:
+            print( 'We are in web2py and in a web request, client IP is [%s]' % current.request.client )
+        else:
+            print( 'We are in web2py, but no web request found' )
+    else:
+        print( 'Not running in web2py!' );
+
+    # args should be a tuple (strings passed on the command line)
     if len(args) < 2:
         print('Usage: github_client.py AUTH_TOKEN')
         return
 
     # expecting OAuth token as second argument
+    print('Quick test of API call and auth token...')
     token = args[1]
+    try:
+        gh = Github( token )
+        gh_user = gh.get_user()
+        TEST_login = gh_user.login   # needed to raise bad-credential exception
+    except BadCredentialsException, e:
+        print('Bad credentials! Please proofread (or refresh) the auth token.')
+        return
+    except GithubException, e:
+        print('Something else went wrong (too many login attempts?)')
+        return
+    except Exception, e:
+        print('Something else went wrong <%s>: %s' % (type(e), e,))
+        return
 
-    if in_web2py:
-        if current:
-            pprint( 'We are in a web request, client IP is [%s]' % current.request.client )
-        else:
-            pprint( 'No web request found' )
-    else:
-        pprint( 'Not running in web2py!' );
-
-    output = fetch_study(23, {'token': token})
-    pprint( output )
+    print('Current Github API status: %s' % (gh.get_api_status().status,))
+    print('This token is for user [%s] "%s <%s>"' % (gh_user.login, gh_user.name, gh_user.email,))
+    print('Repos for this user:')
+    for repo in gh_user.get_repos():
+        print '  %s' % repo.name
+    print('Rate limiting: %s' % (gh.rate_limiting,) );
+    print('Rate limiting reset time: %s' % (gh.rate_limiting_resettime,) );
 
 
 __name__ == '__main__' and main(*sys.argv)
