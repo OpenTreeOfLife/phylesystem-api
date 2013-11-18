a72b20a7c614c86a196585a6703b218613f6d74b
Jonathan "Duke" Leto <jonathan@leto.net>
2013-11-18 14:51:58 -0800

    Add a simple search controller which wraps around the Github code search API

--- modules/githubsearch.py
+++ modules/githubsearch.py
@@ -6,7 +6,9 @@ class GithubSearch(object):
         self.api_url = "https://api.github.com/search/code?q="
         self.repo    = "OpenTreeOfLife/treenexus"
     def search(self,term):
-        search_url = "%s+repo:%s" % (self.api_url, self.repo)
+        search_url = "%s%s+repo:%s" % (self.api_url, term, self.repo)
+        print "Requesting %s" % search_url
         r = requests.get(search_url)
         if r.ok:
             search_json = json.loads(r.text or r.content)
+            return search_json
