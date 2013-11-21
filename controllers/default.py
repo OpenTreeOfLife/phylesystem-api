4a07322086ce4abae47899c733b68fd17ebac181
Jim Allman <jim@ibang.com>
2013-11-21 15:08:25 -0500

    Adding simple CORS support (no tweaks to web2py)
    This should respond to CORS "preflight" requests, allowing any domain to use the OTOL API. If we have problems with this, we might need to patch web2py/gluon/globals.py to allow the 'OPTIONS' request_method.

--- controllers/default.py
+++ controllers/default.py
@@ -13,6 +13,11 @@ def v1():
     "The OpenTree API v1"
     response.view = 'generic.json'
 
+    # CORS support for cross-domain API requests (from anywhere)
+    response.headers['Access-Control-Allow-Origin'] = "*"
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Max-Age'] = 86400  # cache for a day
+
     def __validate(nexson):
         # stub function for hooking into NexSON validation
         pass
@@ -150,5 +155,13 @@ def v1():
             response.view = 'generic.jsonp'
 
         if not resource=='study': raise HTTP(400, 'resource != study')
+
+    def OPTIONS(args, **kwargs):
+        "A simple method for approving CORS preflight request"
+        if request.env.http_access_control_request_method:
+             response.headers['Access-Control-Allow-Methods'] = request.env.http_access_control_request_method
+        if request.env.http_access_control_request_headers:
+             response.headers['Access-Control-Allow-Headers'] = request.env.http_access_control_request_headers
+        raise HTTP(200)
 
     return locals()
