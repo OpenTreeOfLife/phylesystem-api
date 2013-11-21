156e040904920a0c199b13de335a9bced5294a66
Jonathan "Duke" Leto <jonathan@leto.net>
2013-11-21 12:09:09 -0800

    Implement locally writing to a study and committing the result

--- modules/gitdata.py
+++ modules/gitdata.py
@@ -1,3 +1,5 @@
+from sh import git
+import os
 
 class GitData(object):
     def __init__(self, repo):
@@ -10,3 +12,22 @@ class GitData(object):
         except:
             return ''
         return file.read()
+
+    def write_study(self,study_id, content, author="OpenTree API <api@opentreeoflife.org>"):
+        study_filename = "study/%s/%s.json" % (study_id, study_id)
+        # TODO: create the containing directory if we are writing a new study
+        file = open("%s/%s" % (self.repo,study_filename), 'w')
+        file.write(content)
+        file.close()
+
+        orig_cwd = os.getcwd()
+        os.chdir(self.repo)
+
+        git.add(study_filename)
+        git.commit(author=author, message="Update Study #%s via OpenTree API" % study_id)
+
+        os.chdir(orig_cwd)
+
+    def push(self):
+        # TODO: set up GIT_SSH to use proper deployment key for repo
+        git.push()
