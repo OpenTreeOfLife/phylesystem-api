76407213fa73a097133046c90c87f3e23070c412
Mark T. Holder <mtholder@gmail.com>
2013-11-21 23:22:46 -0600

    untested code for conversion of XML to a dictionary in get_ot_study_info_from_treebase_nexml and get_ot_study_info_from_nexml

--- modules/nexson_validator.py
+++ modules/nexson_validator.py
@@ -1,10 +1,125 @@
 #!/usr/bin/env python
-import sys
-import codecs
+import xml.etree.ElementTree as ET
 from cStringIO import StringIO
+import codecs
+import json
+import sys
 import re
 
 VERSION = '0.0.2a'
+
+###############################################################################
+# Code for badgerfish conversion of TreeBase XML to 
+###############################################################################
+def _hacky_strip_namespace(s):
+    '''This is the hacky function used in badgerfish conversion that uses '{ns}tag'
+    as the pattern for a tag with namespace `ns`
+    '''
+    if s.startswith('{'):
+        sl = s.split('}') # hack to deal with namespaces...
+        assert len(sl) == 2
+        return sl[-1]
+    return s
+
+def _gen_bf_el(x):
+    '''
+    Builds a dictionary from the ElementTree element x
+    The function
+    Uses as hacky splitting of attribute or tag names using {}
+        to remove namespaces.
+    returns a pair of: the tag of `x` and the badgerfish
+        representation of the subelements of x
+    '''
+    obj = {}
+    # grab the tag of x
+    t = _hacky_strip_namespace(x.tag)
+    # add the attributes to the dictionary
+    a = x.attrib
+    for k, v in a.iteritems():
+        obj['@' + _hacky_strip_namespace(k)] = v
+    # store the text content of the element under the key '$'
+    if x.text:
+        text_content = x.text.strip()
+    else:
+        text_content = ''
+    if text_content:
+        obj['$'] = text_content
+    # accumulate a list of the children names in ko, and 
+    #   the a dictionary of tag to xml elements.
+    # repetition of a tag means that it will map to a list of
+    #   xml elements
+    cd = {}
+    ko = []
+    ks = set()
+    for child in x:
+        k = _hacky_strip_namespace(child.tag)
+        if k not in ks:
+            ko.append(k)
+            ks.add(k)
+        p = cd.get(k)
+        if p is None:
+            cd[k] = child
+        elif isinstance(p, list):
+            p.append(child)
+        else:
+            cd[k] = [p, child]
+    # Converts the child XML elements to dicts by recursion and
+    #   adds these to the dict.
+    for k in ko:
+        v = cd[k]
+        if isinstance(v, list):
+            dcl = []
+            ct = None
+            for xc in v:
+                ct, dc = _gen_bf_el(xc)
+                dcl.append(dc)
+        else:
+            ct, dcl = _gen_bf_el(v)
+        # this assertion will trip is the hacky stripping of namespaces
+        #   results in a name clash among the tags of the children
+        assert ct not in obj
+        obj[ct] = dcl
+    return t, obj
+
+def to_badgerfish_dict(filepath=None, file_object=None, encoding=u'utf8'):
+    '''Takes either:
+            (1) a file_object, or
+            (2) (if file_object is None) a filepath and encoding
+    Returns a dictionary with the keys/values encoded according to the badgerfish convention
+    See http://badgerfish.ning.com/
+
+    Caveats/bugs:
+        
+    '''
+    if file_object is None:
+        file_object = codecs.open(filepath, 'rU', encoding=encoding)
+    root = ET.parse(file_object).getroot()
+    key, val = _gen_bf_el(root)
+    return {key: val}
+
+def get_ot_study_info_from_nexml(filepath=None, file_object=None, encoding=u'utf8'):
+    '''Converts an XML doc to JSON using the badgerfish convention (see to_badgerfish_dict)
+    and then prunes elements not used by open tree of life study curartion.
+
+    Currently:
+        removes nexml/characters @TODO: should replace it with a URI for 
+            where the removed character data can be found.
+    '''
+    o = to_badgerfish_dict(fn)
+    del o['nexml']['characters']
+    return o
+
+def get_ot_study_info_from_treebase_nexml(filepath=None, file_object=None, encoding=u'utf8'):
+    '''Just a stub at this point. Intended to normalize treebase-specific metadata 
+    into the locations where open tree of life software that expects it. 
+    @TODO: need to investigate which metadata should move or be copied
+    '''
+    o = get_ot_study_info_from_nexml(filepath=filepath, file_object=file_object, encoding=encoding)
+    return o
+
+################################################################################
+# End of badgerfish...
+################################################################################
 
 
 class NexSONError(Exception):
