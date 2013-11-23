253cb420fe4c466da4165b5bbd35ed89d68bafa2
Jonathan "Duke" Leto <jonathan@leto.net>
2013-11-22 17:31:38 -0800

    Improve test assumptinos and add proper cleanup methods to GitData tests
    Now these tests do not depend on already-existing branch names in our
    test git repo and delete branches which are created in the process of
    testing.

--- modules/test_gitdata.py
+++ modules/test_gitdata.py
@@ -1,12 +1,30 @@
 import unittest
 import os
 import sys
+import time
 from gitdata import GitData
 import simplejson as json
+from sh import git
 
 class TestGitData(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.repo = "/Users/jleto/git/opentree/treenexus"
+        self.orig_cwd = os.getcwd()
+
+        # go into our data repo
+        os.chdir(self.repo)
+
+        self.testing_branch_name = "testing_%d" % int(time.time())
+        # create the branch
+        git.checkout("-b", self.testing_branch_name)
+
+    @classmethod
+    def tearDownClass(self):
+
+        git.branch("-D",self.testing_branch_name)
+
+        os.chdir(self.orig_cwd)
 
     def test_fetch(self):
         gd = GitData(repo=self.repo)
@@ -21,6 +39,12 @@ class TestGitData(unittest.TestCase):
         self.assertTrue( valid, "fetch_study(%s) returned valid JSON" % study_id)
 
     def test_write(self):
+        def cleanup_write():
+            git.checkout("master")
+            git.branch("-D","johndoe_study_9999")
+
+        self.addCleanup(cleanup_write)
+
         gd = GitData(repo=self.repo)
         author   = "John Doe <john@doe.com>"
         content  = '{"foo":"bar"}'
@@ -29,12 +53,14 @@ class TestGitData(unittest.TestCase):
         new_sha  = gd.write_study(study_id,content,branch,author)
         self.assertTrue( new_sha != "", "new_sha is non-empty")
 
+
     def test_branch_exists(self):
         gd = GitData(repo=self.repo)
         exists = gd.branch_exists("nothisdoesnotexist")
         self.assertTrue( exists == 0, "branch does not exist")
 
-        branch_name = "only_here"
+        branch_name = self.testing_branch_name
+
         exists = gd.branch_exists(branch_name)
         self.assertTrue( exists, "%s branch exists" % branch_name)
 
