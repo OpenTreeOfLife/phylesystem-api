4162cb28ce485003e6637b3c01f5bdefb9c84bbd
Jonathan "Duke" Leto <jonathan@leto.net>
2013-11-26 17:57:28 -0800

    Get study deletion working and add a test

---
+++ ws-tests/test_study_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys, os
+from opentreetesting import test_http_json_method, config
+DOMAIN = config('host', 'apihost')
+SUBMIT_URI = DOMAIN + '/v1/study/9'
+data = {
+         'auth_token': os.environ.get('GITHUB_OAUTH_TOKEN', 'bogus_token'),
+}
+if test_http_json_method(SUBMIT_URI, 'DELETE',data=data, expected_status=200):
+    sys.exit(0)
+sys.exit(1)
