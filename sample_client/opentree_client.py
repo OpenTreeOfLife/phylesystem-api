1b1db16bcbad1cb4ff7ce440af485a2d05e5b405
Jonathan "Duke" Leto <jonathan@leto.net>
2013-12-05 13:06:22 -0800

    Add some examples of reading and modifying studies via the API from Python

---
+++ sample_client/opentree_client.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import requests
+import codecs
+import json
+import sys
+import os
+
+# Some of these examples require a Github OAUTH token stored in
+# the environment variable GITHUB_OAUTH_TOKEN
+# See
+# https://github.com/OpenTreeOfLife/api.opentreeoflife.org#using-the-api
+# for details about generating the token
+
+if len(sys.argv) > 1:
+    apihost = sys.argv[1]
+else:
+    apihost = "http://127.0.0.1:8000"
+
+headers = {
+    'content-type' : 'application/json',
+    'accept' : 'application/json',
+}
+
+def example_get(study_id=9):
+    url  = "%s/api/default/%s%s" % (apihost, "v1/study/", study_id)
+    print "Requesting GET %s" % url
+    resp = requests.get(url, headers=headers, allow_redirects=True)
+    print "Got status code: %s" % resp.status_code
+
+    try:
+        json_response = resp.json()
+        print json_response
+    except:
+        text = resp.text
+        print text
+
+def example_put(study_id=9):
+    url  = "%s/api/default/%s%s" % (apihost, "v1/study/", study_id)
+    print "Requesting PUT %s" % url
+
+    study_file = codecs.open("study.json", "rU", encoding="utf-8")
+    nexson     = json.load(study_file)
+
+    data = { 'nexson' : nexson,
+            'auth_token': os.environ.get('GITHUB_OAUTH_TOKEN', 'bogus_token'),
+    }
+
+    resp = requests.put(url, data=json.dumps(data), headers=headers, allow_redirects=True)
+
+    print "Got status code: %s" % resp.status_code
+
+    try:
+        json_response = resp.json()
+        print json_response
+    except:
+        text = resp.text
+        print text
+
+# Read a study
+example_get()
+
+# Modify a study
+example_put()
