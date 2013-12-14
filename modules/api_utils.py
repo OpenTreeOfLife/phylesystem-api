cb18bc5d7bf8065a5913206444c5159a25f9dfee
Jonathan "Duke" Leto <jonathan@leto.net>
2013-12-13 16:32:34 -0800

    Implement a working merge controller
    Example usage via curl:
    
        # merge branch leto_study_1003 to master
        curl -X POST http://localhost:8000/api/merge/v1/leto_study_1003?auth_token=$GITHUB_OAUTH_TOKEN
    
        # Be explicit about base branch to merge into, i.e. merge leto_study_12 into master
        curl -X POST http://localhost:8000/api/merge/v1/leto_study_12/master?auth_token=$GITHUB_OAUTH_TOKEN

--- modules/api_utils.py
+++ modules/api_utils.py
@@ -1,5 +1,10 @@
 from ConfigParser import SafeConfigParser
+from github import Github, BadCredentialsException
 import os
+import json
+
+# this allows us to raise HTTP(...)
+from gluon import *
 
 def read_config(request):
     app_name = "api"
@@ -18,3 +23,37 @@ def read_config(request):
     pkey        = conf.get("apis", "pkey")
 
     return repo_path, repo_remote, git_ssh, pkey
+
+def authenticate(**kwargs):
+    # this is the GitHub API auth-token for a logged-in curator
+    auth_token   = kwargs.get('auth_token','')
+
+    if not auth_token:
+        raise HTTP(400,json.dumps({
+            "error": 1,
+            "description":"You must provide an auth_token to authenticate to the OpenTree API"
+        }))
+    gh           = Github(auth_token)
+    gh_user      = gh.get_user()
+
+    try:
+        gh_user.login
+    except BadCredentialsException:
+        raise HTTP(400,json.dumps({
+            "error": 1,
+            "description":"You have provided an invalid or expired authentication token"
+        }))
+
+    author_name  = kwargs.get('author_name','')
+    author_email = kwargs.get('author_email','')
+
+    # use the Github Oauth token to get a name/email if not specified
+    # we don't provide these as default values above because they would
+    # generate API calls regardless of author_name/author_email being specifed
+
+    if not author_name:
+        author_name = gh_user.name
+    if not author_email:
+        author_email = gh_user.email
+
+    return gh, author_name, author_email
