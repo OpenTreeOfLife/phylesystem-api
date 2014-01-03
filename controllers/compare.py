a3926d3b5f2f6ac1ee32d3227b1f98534ed1c41e
Jonathan "Duke" Leto <jonathan@leto.net>
2014-01-02 22:06:29 -0800

    Remove github_client which was mostly dead code and update compare controller accordingly

--- controllers/compare.py
+++ controllers/compare.py
@@ -1,7 +1,7 @@
 import os
 import time
 import json
-import github_client
+from api_utils import authenticate
 
 @request.restful()
 def v1():
@@ -23,6 +23,37 @@ def v1():
             if auth_header:
                 auth_token = auth_header.split()[1]
 
-        return github_client.compare("OpenTreeOfLife/treenexus",base,head, auth_token)
+        return _compare("OpenTreeOfLife/treenexus",base,head, auth_token)
+
+    def _compare(repo_path, base, head, token):
+        """Compare two commits (by SHA or branch name) and return a list of changed files
+
+        Example URL: http://localhost:8080/api/compare/v1/c3608e5/master
+
+        which calls out to https://api.github.com/repos/OpenTreeOfLife/treenexus/compare/c3608e5...master
+
+    """
+        (gh,gh_user,gh_username) = authenticate(auth_token=token)
+
+        repo = gh.get_repo(repo_path)
+
+        comparison = repo.compare(base,head)
+
+        excludes = [ ".to_download.json" ]
+
+        files         = [ f for f in comparison.files if f.filename not in excludes ]
+        changed_files = [ f.filename for f in files if f.status == "modified" ]
+        added_files   = [ f.filename for f in files if f.status == "added"    ]
+        removed_files = [ f.filename for f in files if f.status == "removed"  ]
+        renamed_files = [ f.filename for f in files if f.status == "renamed"  ]
+
+        return json.dumps(
+            {
+                "changed": changed_files,
+                "added":   added_files,
+                "removed": removed_files,
+                "renamed": renamed_files,
+            }
+        )
 
     return locals()
