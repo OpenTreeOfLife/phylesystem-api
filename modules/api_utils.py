6912b5be4b4c7dc80da9316fcb09617aea09aa5b
Mark T. Holder <mtholder@gmail.com>
2014-02-09 23:10:06 -0600

    config defaults for git_ssh and pkey. and logger

--- modules/api_utils.py
+++ modules/api_utils.py
@@ -1,5 +1,6 @@
 from ConfigParser import SafeConfigParser
 from github import Github, BadCredentialsException
+import logging
 import os
 import json
 
@@ -19,8 +20,14 @@ def read_config(request):
 
     repo_path   = conf.get("apis","repo_path")
     repo_remote = conf.get("apis", "repo_remote")
-    git_ssh     = conf.get("apis", "git_ssh")
-    pkey        = conf.get("apis", "pkey")
+    try:
+        git_ssh     = conf.get("apis", "git_ssh")
+    except:
+        git_ssh = 'ssh'
+    try:
+        pkey        = conf.get("apis", "pkey")
+    except:
+        pkey = None
 
     return repo_path, repo_remote, git_ssh, pkey
 
@@ -69,3 +76,72 @@ def authenticate(**kwargs):
         author_email = gh_user.email
 
     return gh, author_name, author_email
+
+
+_LOGGING_LEVEL_ENVAR="OT_API_LOGGING_LEVEL"
+_LOGGING_FORMAT_ENVAR="OT_API_LOGGING_FORMAT"
+
+def _get_logging_level():
+    if _LOGGING_LEVEL_ENVAR in os.environ:
+        if os.environ[_LOGGING_LEVEL_ENVAR].upper() == "NOTSET":
+            level = logging.NOTSET
+        elif os.environ[_LOGGING_LEVEL_ENVAR].upper() == "DEBUG":
+            level = logging.DEBUG
+        elif os.environ[_LOGGING_LEVEL_ENVAR].upper() == "INFO":
+            level = logging.INFO
+        elif os.environ[_LOGGING_LEVEL_ENVAR].upper() == "WARNING":
+            level = logging.WARNING
+        elif os.environ[_LOGGING_LEVEL_ENVAR].upper() == "ERROR":
+            level = logging.ERROR
+        elif os.environ[_LOGGING_LEVEL_ENVAR].upper() == "CRITICAL":
+            level = logging.CRITICAL
+        else:
+            level = logging.NOTSET
+    else:
+        level = logging.NOTSET
+    return level
+
+def get_logger(name="ot_api"):
+    """
+    Returns a logger with name set as given, and configured
+    to the level given by the environment variable _LOGGING_LEVEL_ENVAR.
+    """
+
+#     package_dir = os.path.dirname(module_path)
+#     config_filepath = os.path.join(package_dir, _LOGGING_CONFIG_FILE)
+#     if os.path.exists(config_filepath):
+#         try:
+#             logging.config.fileConfig(config_filepath)
+#             logger_set = True
+#         except:
+#             logger_set = False
+    logger = logging.getLogger(name)
+    if not hasattr(logger, 'is_configured'):
+        logger.is_configured = False
+    if not logger.is_configured:
+        level = _get_logging_level()
+        rich_formatter = logging.Formatter("[%(asctime)s] %(filename)s (%(lineno)d): %(levelname) 8s: %(message)s")
+        simple_formatter = logging.Formatter("%(levelname) 8s: %(message)s")
+        raw_formatter = logging.Formatter("%(message)s")
+        default_formatter = None
+        logging_formatter = default_formatter
+        if _LOGGING_FORMAT_ENVAR in os.environ:
+            if os.environ[_LOGGING_FORMAT_ENVAR].upper() == "RICH":
+                logging_formatter = rich_formatter
+            elif os.environ[_LOGGING_FORMAT_ENVAR].upper() == "SIMPLE":
+                logging_formatter = simple_formatter
+            elif os.environ[_LOGGING_FORMAT_ENVAR].upper() == "NONE":
+                logging_formatter = None
+            else:
+                logging_formatter = default_formatter
+        else:
+            logging_formatter = default_formatter
+        if logging_formatter is not None:
+            logging_formatter.datefmt='%H:%M:%S'
+        logger.setLevel(level)
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        ch.setFormatter(logging_formatter)
+        logger.addHandler(ch)
+        logger.is_configured = True
+    return logger
