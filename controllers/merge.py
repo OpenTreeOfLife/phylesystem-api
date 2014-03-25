6aa5c7bf43059376feeaafbeb7dca8a67ba54c9d
Emily Jane McTavish <ejmctavish@gmail.com>
2014-03-25 14:33:14 -0500

    added request to phylesystem calls

--- controllers/merge.py
+++ controllers/merge.py
@@ -46,8 +46,8 @@ def v1():
         if jsoncallback or callback:
             response.view = 'generic.jsonp'
         auth_info = api_utils.authenticate(**kwargs)
-        phylesystem = api_utils.get_phylesystem()
-        gd = phylesystem.create_git_action()
+        phylesystem = api_utils.get_phylesystem(request)
+        gd = phylesystem.create_git_action(resource_id)
         try:
             return merge_from_master(gd, resource_id, auth_info, starting_commit_SHA)
         except GitWorkflowError, err:
