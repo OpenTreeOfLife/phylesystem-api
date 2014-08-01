bb52e65a777e1a0b7b4c72ba3fc5ccc0600048a2
Jim Allman <jim@ibang.com>
2014-08-01 17:08:33 -0400

    Offer sensible versions of CC-BY (not 4.0)
    Addressed in https://github.com/OpenTreeOfLife/opentree/issues/369

--- controllers/default.py
+++ controllers/default.py
@@ -428,8 +428,19 @@ def v1():
                 if existing_license == 'CC-0':
                     nexml['^xhtml:license'] = {'@href': 'http://creativecommons.org/publicdomain/zero/1.0/'}
                     pass
+                elif existing_license == 'CC-BY-2.0':
+                    nexml['^xhtml:license'] = {'@href': 'http://creativecommons.org/licenses/by/2.0/'}
+                    pass
+                elif existing_license == 'CC-BY-2.5':
+                    nexml['^xhtml:license'] = {'@href': 'http://creativecommons.org/licenses/by/2.5/'}
+                    pass
+                elif existing_license == 'CC-BY-3.0':
+                    nexml['^xhtml:license'] = {'@href': 'http://creativecommons.org/licenses/by/3.0/'}
+                    pass
+                # NOTE that we don't offer CC-BY 4.0, which is problematic for data
                 elif existing_license == 'CC-BY':
-                    nexml['^xhtml:license'] = {'@href': 'http://creativecommons.org/licenses/by/4.0/'}
+                    # default to version 3, if not specified. 
+                    nexml['^xhtml:license'] = {'@href': 'http://creativecommons.org/licenses/by/3.0/'}
                     pass
                 else:  # assume it's something else
                     alt_license_name = kwargs.get('alt_license_name', '')
