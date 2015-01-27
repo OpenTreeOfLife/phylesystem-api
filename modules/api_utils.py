374093c658c956adb141f4eedc032ed0a570184e
Emily Jane McTavish <ejmctavish@gmail.com>
2015-01-27 11:46:43 +0100

    overrides max filesize in peyotl config to prevent oversize commits

--- modules/api_utils.py
+++ modules/api_utils.py
@@ -1,7 +1,8 @@
 from github import Github, BadCredentialsException
 from peyotl.nexson_syntax import write_as_json
 from peyotl.phylesystem import Phylesystem
-from peyotl.utility import read_config, get_config
+from peyotl.utility import read_config as read_peyotl_config
+from peyotl.utility import get_config as get_peyotl_config
 #from peyotl.utility import get_logger, read_config, _replace_default_config
 from ConfigParser import SafeConfigParser
 from datetime import datetime
@@ -42,16 +43,18 @@ def compose_push_to_github_url(request, resource_id):
 # this allows us to raise HTTP(...)
 from gluon import *
 _PHYLESYSTEM = None
-config, cfg_filename = read_config()
-if 'phylesystem' not in config.sections():
-        config.add_section('phylesystem')
-config.set('phylesystem', 'max_file_size', '10') #overrides peyotl config with max phylesytem-api filesize
+
+
 def get_phylesystem(request):
     global _PHYLESYSTEM
     if _PHYLESYSTEM is not None:
         return _PHYLESYSTEM
     from gitdata import GitData
-    repo_parent, repo_remote, git_ssh, pkey, git_hub_remote = read_config(request)
+    repo_parent, repo_remote, git_ssh, pkey, git_hub_remote, max_filesize, max_num_trees = read_config(request)
+    peyotl_config, cfg_filename = read_peyotl_config()
+    if 'phylesystem' not in peyotl_config.sections():
+        peyotl_config.add_section('phylesystem')
+    peyotl_config.set('phylesystem', 'max_file_size', max_filesize) #overrides peyotl config with max phylesytem-api filesize
     push_mirror = os.path.join(repo_parent, 'mirror')
     pmi = {
         'parent_dir': push_mirror,
@@ -108,7 +111,15 @@ def read_config(request):
         git_hub_remote = conf.get("apis", "git_hub_remote")
     except:
         git_hub_remote = 'git@github.com:OpenTreeOfLife'
-    return repo_parent, repo_remote, git_ssh, pkey, git_hub_remote
+    try:
+        max_filesize = conf.get("filesize", "peyotl_max_file_size")
+    except:
+        max_filesize = '20000000'
+    try:
+        max_num_trees = conf.get("filesize", "validation_max_num_trees")
+    except:
+        max_num_trees = '65'   
+    return repo_parent, repo_remote, git_ssh, pkey, git_hub_remote, max_filesize, max_num_trees
 
 def read_logging_config(request):
     conf = get_conf_object(request)
