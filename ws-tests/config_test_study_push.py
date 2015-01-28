af0fb44f5c2ae0c3b8f3166ae4899f1bd1eeabc0
Emily Jane McTavish <ejmctavish@gmail.com>
2015-01-28 12:26:40 +0100

    some tests for alternate configuration

---
+++ ws-tests/config_test_study_push.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import sys, os
+from opentreetesting import test_http_json_method, config
+#This test should only pass if filesize is set very low in the config and/or max tree number is set to 0
+DOMAIN = config('host', 'apihost')
+study = '10'
+SUBMIT_URI = DOMAIN + '/v1/study/' + study
+data = {'output_nexml2json':'1.2'}
+r = test_http_json_method(SUBMIT_URI,
+                          'GET',
+                          data=data,
+                          expected_status=200,
+                          return_bool_data=True)
+d = r[1]['data']
+c = d['nexml'].get('^ot:testCount', 0)
+if isinstance(c, list):
+    c = c[0]
+c = c + 1
+d['nexml']['^ot:testCount'] = c
+
+starting_commit_SHA = r[1]['sha']
+data = { 'nexson' : d,
+         'auth_token': os.environ.get('GITHUB_OAUTH_TOKEN', 'bogus_token'),
+         'starting_commit_SHA': starting_commit_SHA,
+}
+r2 = test_http_json_method(SUBMIT_URI,
+                           'PUT',
+                           data=data,
+                           expected_status=400,
+                           return_bool_data=True)
+
+PUSH_URI = DOMAIN + '/push/v1/' + study
+r3 = test_http_json_method(PUSH_URI,
+                           'PUT',
+                           expected_status=400,
+                           return_bool_data=True)
+print r3
