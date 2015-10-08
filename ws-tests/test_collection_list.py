7fcabdb2e2c41084fb5dfb496376171726caa436
Jim Allman <jim@ibang.com>
2015-10-08 16:48:11 -0400

    Add initial ws-tests for tree collections.

---
+++ ws-tests/test_collection_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys, os
+from opentreetesting import test_http_json_method, config
+DOMAIN = config('host', 'apihost')
+SUBMIT_URI = DOMAIN + '/v2/collections/find_collections'
+#sys.stderr.write('Calling "{}"...\n'.format(SUBMIT_URI))
+r = test_http_json_method(SUBMIT_URI,
+                          'GET',
+                          expected_status=200,
+                          return_bool_data=True)
+if not r[0]:
+    sys.exit(1)
+#print r[1]
