295454051911639794da9daebb05fda920b158d7
Jim Allman <jim@ibang.com>
2016-08-18 12:11:20 -0400

    Add simple amendment tests (from collections)

---
+++ ws-tests/test_amendment_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys, os
+from opentreetesting import test_http_json_method, config
+DOMAIN = config('host', 'apihost')
+SUBMIT_URI = DOMAIN + 'v3/amendments/list_all'
+r = test_http_json_method(SUBMIT_URI,
+                          'GET',
+                          expected_status=200,
+                          return_bool_data=True)
+if not r[0]:
+    sys.exit(1)
+#print r[1]
