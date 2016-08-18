193ef7e8e4f6629e8534083e705826656c908cab
Jim Allman <jim@ibang.com>
2016-08-18 12:18:03 -0400

    Use typical domain value (no trailing slash)

--- ws-tests/test_amendment_list.py
+++ ws-tests/test_amendment_list.py
@@ -2,7 +2,7 @@
 import sys, os
 from opentreetesting import test_http_json_method, config
 DOMAIN = config('host', 'apihost')
-SUBMIT_URI = DOMAIN + 'v3/amendments/list_all'
+SUBMIT_URI = DOMAIN + '/v3/amendments/list_all'
 r = test_http_json_method(SUBMIT_URI,
                           'GET',
                           expected_status=200,
