d1ff434f6efbade9194b2a0cf67e1128e78eef03
Jim Allman <jim@ibang.com>
2021-08-03 15:57:26 -0400

    Add a simple home page for API server

--- phylesystem_api/phylesystem_api/routes.py
+++ phylesystem_api/phylesystem_api/routes.py
@@ -1,3 +1,4 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('home', '/')  # tiny README for the curious
+    # config.add_route('task_detail', '/api/v1/accounts/{username}/tasks/{id}')
