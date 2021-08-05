4da6a27060fad9d6272931d438576b478f2a5e00
Jim Allman <jim@ibang.com>
2021-08-05 15:41:18 -0400

    Add proper phylesystem method (requires peyotl)

--- phylesystem_api/phylesystem_api/views/default.py
+++ phylesystem_api/phylesystem_api/views/default.py
@@ -7,9 +7,15 @@ def home_view(request):
     # a simple README web page for the curious
     return {'title': 'phylesystem API'}
 
-@view_config(route_name='{api_version}/render_markdown')
+@view_config(route_name='render_markdown')
 def render_markdown(request):
     # Convert POSTed Markdown to HTML (e.g., for previews in web UI)
     src = request.body.decode('utf-8')
     html = _markdown_to_html( src, open_links_in_new_window=True )
     return Response(body=html, content_type='text/html')
+
+@view_config(route_name='phylesystem_config', renderer='json')
+def phylesystem_config(request):
+    # general information about the hosted phylesystem
+    phylesystem = api_utils.get_phylesystem(request)
+    return phylesystem.get_configuration_dict()
