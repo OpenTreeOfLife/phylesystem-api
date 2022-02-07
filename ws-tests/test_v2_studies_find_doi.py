9ce1ba2c5cc2a4eab0f774e848dad780cafc5263
Jim Allman <jim@ibang.com>
2022-02-07 14:13:19 -0500

    Update to v3 response and URL-ified DOI

--- ws-tests/test_v2_studies_find_doi.py
+++ ws-tests/test_v2_studies_find_doi.py
@@ -2,17 +2,18 @@
 import sys, os
 from opentreetesting import test_http_json_method, config
 DOMAIN = config('host', 'apihost')
-CONTROLLER = DOMAIN + '/phylesystem/studies'
+CONTROLLER = DOMAIN + '/v3/studies'
 SUBMIT_URI = CONTROLLER + '/find_studies'
 p = {'verbose': True,
      'property': 'ot:studyPublication',
-     'value': '10.1600/036364408785679851',}
+     'value': 'http://dx.doi.org/10.1600/036364408785679851',}
 # DOI was formerly 10.3732/ajb.94.11.1860
 r = test_http_json_method(SUBMIT_URI,
-                          'GET',
+                          'POST',
                           data=p,
                           expected_status=200,
                           return_bool_data=True)
 assert r[0] is True
 assert len(r[1]) > 0
-assert 'ot:studyId' in list(r[1][0].keys())
+assert len(r[1]['matched_studies']) > 0
+assert 'ot:studyId' in list(r[1]['matched_studies'][0].keys())
