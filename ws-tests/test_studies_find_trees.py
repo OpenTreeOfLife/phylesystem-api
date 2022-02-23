c1d04de825de53aaf89c8f4badaf869594fbc507
Jim Allman <jim@ibang.com>
2022-02-23 16:48:01 -0500

    Revert to expecting a simpler (old) response

--- ws-tests/test_studies_find_trees.py
+++ ws-tests/test_studies_find_trees.py
@@ -14,5 +14,4 @@ r = test_http_json_method(SUBMIT_URI,
                           return_bool_data=True)
 assert r[0] is True
 assert len(r[1]) > 0
-assert len(r[1]['matched_studies']) > 0
-assert 'ot:studyId' in list(r[1]['matched_studies'][0].keys())
+assert 'ot:studyId' in r[1][0]
