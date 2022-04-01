9f30892846f5ecea94c54d7682f95d5399b8a8c5
Jim Allman <jim@ibang.com>
2022-04-01 17:15:48 -0400

    Handle optional final slash for all REST methods
    This will redirect from any no-slash URLs to the canonical version with
    slashes. (See the old `_noslash` form if we want to avoid these
    redirects in each case.)

--- phylesystem_api/phylesystem_api/routes.py
+++ phylesystem_api/phylesystem_api/routes.py
@@ -1,16 +1,10 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    """
-    config.add_notfound_view(append_slash=True,
-                             renderer='json',
-                             accept='application/json')
-    """
     # show a simple HTML homepage for the curious
     config.add_route('index', '/')
     # some (unused) URLs just show a tiny API description, with links to code + docs
     config.add_route('api_root', '/')
     config.add_route('api_version_root', '/{api_version}/')
-    config.add_route('api_version_noslash', '/{api_version}')
     config.add_route('studies_root', '/{api_version}/studies/')
     config.add_route('collections_root', '/{api_version}/collections/')
     config.add_route('amendments_root', '/{api_version}/amendments/')
@@ -29,7 +23,7 @@ def includeme(config):
     config.add_route('trees_in_synth', '/{api_version}/trees_in_synth')
     config.add_route('include_tree_in_synth', '/{api_version}/include_tree_in_synth')
     config.add_route('exclude_tree_from_synth', '/{api_version}/exclude_tree_from_synth')
-    config.add_route('create_study', '/{api_version}/study')
+    config.add_route('create_study', '/{api_version}/study/')
     config.add_route('study_CORS_preflight', '/{api_version}/study/{study_id}', request_method='OPTIONS')
     config.add_route('fetch_study', '/{api_version}/study/{study_id}', request_method='GET')
     config.add_route('update_study', '/{api_version}/study/{study_id}', request_method='PUT')
@@ -47,7 +41,7 @@ def includeme(config):
     config.add_route('collection_properties', '/{api_version}/collections/properties')
     config.add_route('find_trees_in_collections', '/{api_version}/collections/find_trees')
     config.add_route('find_collections', '/{api_version}/collections/find_collections')
-    config.add_route('create_collection', '/{api_version}/collection')
+    config.add_route('create_collection', '/{api_version}/collection/')
     config.add_route('collection_CORS_preflight', '/{api_version}/collection/{collection_id}', request_method='OPTIONS')
     config.add_route('fetch_collection', '/{api_version}/collection/{collection_id}', request_method='GET')
     config.add_route('update_collection', '/{api_version}/collection/{collection_id}', request_method='PUT')
@@ -59,7 +53,7 @@ def includeme(config):
     config.add_route('list_all_amendments', '/{api_version}/amendments/list_all')
     config.add_route('get_amendments_config', '/{api_version}/amendments/store_config')
     config.add_route('amendments_push_failure', '/{api_version}/amendments/push_failure')
-    config.add_route('create_amendment', '/{api_version}/amendment')
+    config.add_route('create_amendment', '/{api_version}/amendment/')
     config.add_route('amendment_CORS_preflight', '/{api_version}/amendment/{amendment_id}', request_method='OPTIONS')
     config.add_route('fetch_amendment', '/{api_version}/amendment/{amendment_id}', request_method='GET')
     config.add_route('update_amendment', '/{api_version}/amendment/{amendment_id}', request_method='PUT')
