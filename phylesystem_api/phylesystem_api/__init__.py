edc23a19d927d6a2358d29f174cf9f66b0e8beaa
Jim Allman <jim@ibang.com>
2022-07-25 15:29:17 -0400

    tweak CORS import statement

--- phylesystem_api/phylesystem_api/__init__.py
+++ phylesystem_api/phylesystem_api/__init__.py
@@ -6,8 +6,7 @@ def main(global_config, **settings):
     """
     with Configurator(settings=settings) as config:
         config.include('pyramid_jinja2')
-        #config.include('.cors')  # Python 2
-        config.include('cors')  # Python 3
+        config.include('.cors')
         config.add_cors_preflight_handler()
         config.include('.routes')
         config.scan()
