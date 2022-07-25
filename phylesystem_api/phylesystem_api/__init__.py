6ac546fb5095a16e035b1e115ec01bb40aa0e1a7
Jim Allman <jim@ibang.com>
2022-07-25 15:19:03 -0400

    Add CORS handling for all phylesystem-api calls
    Based on precedent in otindex:
    https://github.com/OpenTreeOfLife/otindex/commit/2180495a34a9450c41627289ec51978e057b3aa5
    
    (and lightly adapted for likely Python 3 import rules)

--- phylesystem_api/phylesystem_api/__init__.py
+++ phylesystem_api/phylesystem_api/__init__.py
@@ -6,6 +6,9 @@ def main(global_config, **settings):
     """
     with Configurator(settings=settings) as config:
         config.include('pyramid_jinja2')
+        #config.include('.cors')  # Python 2
+        config.include('cors')  # Python 3
+        config.add_cors_preflight_handler()
         config.include('.routes')
         config.scan()
     return config.make_wsgi_app()
