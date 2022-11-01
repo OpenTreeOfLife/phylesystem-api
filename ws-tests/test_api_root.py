1ba13a62c09d2d22dea782c2cf20e37c43c8c5ea
snacktavish <ejmctavish@gmail.com>
2022-11-01 14:00:02 -0700

    root doesn't need to return 200

--- ws-tests/test_api_root.py
+++ ws-tests/test_api_root.py
@@ -3,6 +3,6 @@ import sys, os
 from opentreetesting import test_http_json_method, config
 DOMAIN = config('host', 'apihost')
 SUBMIT_URI = '{d}/v3/studies'.format(d=DOMAIN)
-if test_http_json_method(SUBMIT_URI, 'GET', expected_status=200):
+if test_http_json_method(SUBMIT_URI, 'GET', expected_status=404):
     sys.exit(0)
 sys.exit(1)
