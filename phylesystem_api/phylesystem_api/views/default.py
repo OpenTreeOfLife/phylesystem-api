2bd29b4c148213d0540e83e1f206376081c6a4d3
Jim Allman <jim@ibang.com>
2023-05-24 14:18:21 -0400

    Show all headers (something's weird)

--- phylesystem_api/phylesystem_api/views/default.py
+++ phylesystem_api/phylesystem_api/views/default.py
@@ -88,6 +88,8 @@ def pull_through_cache(request):
         fetch_url = "https://devapi.opentreeoflife.org" + root_relative_url
         _LOG.warn("NOT CACHED, FETCHING THIS URL: {}".format(fetch_url))
         _LOG.warn("  request.method = {}".format(request.method))
+        _LOG.warn("  request.headers:")
+        _LOG.warn( request.headers )
         _LOG.warn("  STARTING request Connection header: {}".format( request.headers.get('Connection', '')))
         #proxy_safe_headers = ???
         request.headers['Connection'] = 'Close'
