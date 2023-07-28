d16d652c7c25df923c101413543d1108c5053295
Mark T. Holder <mtholder@gmail.com>
2023-07-28 16:48:30 -0500

    new test

---
+++ ws-tests/test_study_verbose_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys, os
+from opentreetesting import test_http_json_method, config
+
+DOMAIN = config("host", "apihost")
+SUBMIT_URI = DOMAIN + "/v3/studies/find_studies"
+# NB this is not a duplicate of /v3/studies/find_studies !
+# It's a rarely-used method to fetch all studies prior to building the oti index!
+# See this helpful issue from @jar398:
+#   https://github.com/OpenTreeOfLife/germinator/issues/94#issuecomment-227027174
+data = {"verbose": True}
+r = test_http_json_method(SUBMIT_URI, "GET", data=data, expected_status=200, return_bool_data=True)
+if not r[0]:
+    sys.exit(1)
